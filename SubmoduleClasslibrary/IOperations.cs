40a113c986287eba5edb5d236b224a22af399a6d
abhijeet9238 <63091634+abhijeet9238@users.noreply.github.com>
2020-06-21 13:15:40 +0530

    Intial Master

---
+++ SubmoduleClasslibrary/IOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubmoduleClasslibrary
+{
+    public interface IOperations
+    {
+        int add(int value1, int value2);
+        int sub(int value1, int value2);
+        int mul(int value1, int value2);
+        int div(int value1, int value2);
+    }
+}
---
+++ SubmoduleClasslibrary/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubmoduleClasslibrary
+{
+    public class Model
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+        public long result { get; set; }
+    }
+}
