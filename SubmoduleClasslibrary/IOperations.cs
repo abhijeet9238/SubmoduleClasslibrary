0a3156e7987bede6fcc072f78c72809f39a36860
abhijeet9238 <63091634+abhijeet9238@users.noreply.github.com>
2020-06-21 13:52:03 +0530

    OperationClass Added

--- SubmoduleClasslibrary/IOperations.cs
+++ SubmoduleClasslibrary/IOperations.cs
@@ -9,6 +9,6 @@ namespace SubmoduleClasslibrary
         int add(int value1, int value2);
         int sub(int value1, int value2);
         int mul(int value1, int value2);
-        int div(int value1, int value2);
+        long div(int value1, int value2);
     }
 }
---
+++ SubmoduleClasslibrary/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubmoduleClasslibrary
+{
+    public class Operations : IOperations
+    {
+        public int add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        public int div(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int mul(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+       
+
+       
+    }
+}
